f7197d11e074756ec255f18f74660837ae97ad71
ritheesh <mritheesh123@gmail.com>
2024-01-30 13:46:10 +0530

    done

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,20 @@
+package runner;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features=
+	"C:\\Users\\91630\\eclipse-workspace\\ATE-Phase2-Cucumber-Extentreports-Jenkins\\src\\test\\java\\feature\\redifRegister.feature",
+		glue = {"steps"},  // packagename where the steps are
+		plugin= {"pretty","html:target/cucumberreport.html",
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+				"timeline:test-output-thread/"
+		}
+		
+		)
+
+
+public class TestRunner {
+
+}
---
+++ test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"b61e9be0-f648-48b5-b739-1b611bef2f89","feature":"Register multiple users on the Rediff account Page","scenario":"Test rediff Register Account Page","start":1706594792585,"group":1,"content":"","tags":"","end":1706594811835,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[#1,main,5,main]"}]);
+});
