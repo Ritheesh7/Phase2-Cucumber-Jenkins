cb4b382e36fea0fd97738415cd3581304ab146bf
ritheesh <mritheesh123@gmail.com>
2024-01-31 16:29:42 +0530

    updated jdk

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"cddfdf8a-a573-4a0b-b962-d2f4387ef09c","feature":"Register multiple users on the Rediff account Page","scenario":"Test rediff Register Account Page","start":1706615033396,"group":1,"content":"","tags":"","end":1706615050817,"className":"passed"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"1ce2b283-4d34-4feb-8623-edd9218bab09","feature":"Register multiple users on the Rediff account Page","scenario":"Test rediff Register Account Page","start":1706698229223,"group":1,"content":"","tags":"","end":1706698248816,"className":"passed"}]);
 CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[#1,main,5,main]"}]);
 });
